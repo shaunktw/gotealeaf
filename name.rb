6c94a74b95d50c6e83b8edb433a55b47a34d01a2
Shaun Koo <shaunktw@gmail.com>
2014-08-03 21:58:40 -0400

    Added new rb files

---
+++ name.rb
@@ -0,0 +1,14 @@
+puts "Hey what is your name?"
+name = gets.chomp
+puts "Cool! your #{name} is awesome! Welcome #{name}" 
+
+10.times do |n|
+  puts n = name
+end
+
+puts "Hey what is your first name?"
+first_name = gets.chomp
+puts "Okay, now what is your last name?"
+last_name = gets.chomp
+
+puts "Cool shit! Nice to meet you #{first_name} #{last_name}"
